65c1da0e8c740922bce4a7e5311568bc79dde947
Nascimento <nnasci10@ford.com>
2025-05-13 20:53:47 -0300

    Configuring Parcel

---
+++ dist/CODE.900caf0b.js
@@ -0,0 +1,28 @@
+const carregamento = window.onload = function() {
+    console.log("Ol\xe1 Parcel");
+    console.log(carregamento);
+} // ##############################################################
+ // Função para monitorar o carregamento da página
+ // function monitorarCarregamento() {
+ //     // Evento disparado quando a página começa a carregar
+ //     window.addEventListener('loadstart', function() {
+ //         console.log('O carregamento da página começou.');
+ //     });
+ //     // Evento disparado quando a página termina de carregar
+ //     window.addEventListener('load', function() {
+ //         console.log('O carregamento da página foi concluído.');
+ //     });
+ //     // Evento disparado quando há um erro no carregamento da página
+ //     window.addEventListener('load', function(event) {
+ //         console.error('Erro no carregamento da página:', event);
+ //     });
+ //     // Evento disparado quando a página está sendo descarregada
+ //     window.addEventListener('beforeunload', function() {
+ //         console.log('A página está sendo descarregada.');
+ //     });
+ // }
+ // // Chama a função para iniciar o monitoramento
+ // monitorarCarregamento();
+;
+
+//# sourceMappingURL=CODE.900caf0b.js.map
