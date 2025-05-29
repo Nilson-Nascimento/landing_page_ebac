efadf4adc4e6caa909a27a46fc83d3a763972d49
Nascimento <nnasci10@ford.com>
2025-05-28 22:51:05 -0300

    Lading Page done

--- dist/CODE.900caf0b.js
+++ dist/CODE.900caf0b.js
@@ -1,8 +1,47 @@
 const carregamento = window.onload = function() {
     console.log("Ol\xe1 Parcel");
-    console.log(carregamento);
+    // console.log(carregamento)
     AOS.init();
-} // ##############################################################
+};
+const stop = '00:00:00';
+const timeEvent = new Date("Mon May 28 2025 22:47:26");
+const timeStampEvent = timeEvent.getTime();
+// console.log(timeStampEvent + " timeStampEvent")
+const counterHours = setInterval(function() {
+    const now = new Date();
+    const timeStampNow = now.getTime();
+    // console.log(timeStampNow + " timeStampNow")
+    const targetToEvent = timeStampEvent - timeStampNow;
+    console.log(targetToEvent + " targetToEvent");
+    const targetDayEvent = Math.floor(targetToEvent / 86400000);
+    const targetHoursEvent = String(Math.floor(targetToEvent % 86400000 / 3600000)).padStart(2, "0");
+    const targetMinutesEvent = String(Math.floor(targetToEvent % 3600000 / 60000)).padStart(2, "0");
+    const targetSecondsEvent = String(Math.floor(targetToEvent % 60000 / 1000)).padStart(2, "0");
+    // console.log(targetDayEvent + " dias")
+    const counterDown = targetDayEvent + " Dias " + targetHoursEvent + ":" + targetMinutesEvent + ":" + targetSecondsEvent;
+    document.getElementById('counter').innerHTML = "come\xe7a em " + `${counterDown}`;
+    if (targetToEvent < 0) {
+        clearInterval(counterHours);
+        document.getElementById('counter').innerHTML = "Terminou!";
+    }
+}, 1000) // #####################################################################################################
+ // const timeEventHours = String(timeEvent.getHours()).padStart(2, "0")
+ // const timeEventMinutes = String(timeEvent.getMinutes()).padStart(2, "0")
+ // const timeEventSeconds = String(timeEvent.getSeconds()).padStart(2, "0")
+ // const counter = setInterval(function(){
+ //     const timeNow = new Date()
+ //     const conterHours = String(timeNow.getHours()).padStart(2, "0")
+ //     const counterMinutes = String(timeNow.getMinutes()).padStart(2, "0" )
+ //     const counterSeconds = String(timeNow.getSeconds()).padStart(2, "0");
+ //     // parseFloat(timeNow.getSeconds().padStart(2,'0'))
+ //     const counterHoursDown = timeEventHours - conterHours
+ //     const counterMinutesDown = timeEventMinutes - counterMinutes
+ //     const counterSecondsDown = timeEventSeconds - counterSeconds
+ //     const counterDown = String(counterHoursDown).padStart(2, "0") + ":" + String(counterMinutesDown).padStart(2, "0") + ":" + String(counterSecondsDown).padStart(2, "0")
+ //     console.log(counterDown)
+ //     document.getElementById('counter').innerHTML = `${counterDown}`
+ // }, 1000)
+ // ##############################################################
  // Função para monitorar o carregamento da página
  // function monitorarCarregamento() {
  //     // Evento disparado quando a página começa a carregar
